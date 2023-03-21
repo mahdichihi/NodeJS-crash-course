816bf1e64d16f0de4dd60592ccbcae10c310b606
mahdi chihi <mahdichihi95@gmail.com>
2023-03-21 14:43:30 +0100

    end requests & responses

--- server.js
+++ server.js
@@ -1,6 +1,50 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about-me":
+      path += "about.html";
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+  // res.write('<head><link rel="stylesheet" href="#"></head>');
+  // res.write("<p>hello, ninjas</p>");
+  // res.write("<p>hello again, ninjas</p>");
+  // res.end();
+
+  // send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
